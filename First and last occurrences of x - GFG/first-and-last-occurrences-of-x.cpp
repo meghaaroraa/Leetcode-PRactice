d05117a117a37cd3c90c838ce1244bcd5a9c1c8b
Megha Arora <96053499+meghaaroraa@users.noreply.github.com>
2023-09-16 16:36:28 +0530

    Added solution - LeetHub

---
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    vector<int> find(int arr[], int n , int x )
+    {
+        // code here
+        int st = -1;
+        int end = -1;
+        
+        for(int i=0; i<n; i++){
+            if(arr[i] == x){
+                st = i;
+                break;
+            }
+        }
+        
+        for(int i=n-1; i>=0; i--){
+            if(arr[i] == x){
+                end = i;
+                break;
+            }
+        }
+        
+        return {st,end};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        int arr[n],i;
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        vector<int> ans;
+        Solution ob;
+        ans=ob.find(arr,n,x);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
